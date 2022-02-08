cb88012a921605b3e515e94cf10e13758a0ccf18
amitrohan <amit@kruzr.co>
2022-02-08 19:43:15 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [    {provide: APP_BASE_HREF, useValue : '/' },
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
